091ebe4622e405bc177ee8aa268d7073a71ddbb3
leoji <leo.lj.ji@gmail.com>
2014-08-01 22:06:18 +0800

    initiate commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Kato Media Network Plugin
+ * Plugin URI: http://kato.me
+ * Description: Network-wide functions for WordPress
+ * Version: 0.0.1
+ * Author: Leo Ji
+ * Author URI: http://lji.me
+ */
+ 
+if ( function_exists( 'add_image_size' ) ) { 
+	add_image_size( 'medium-large', 800 ); //300 pixels wide (and unlimited height)
+}
+
+add_filter( 'image_size_names_choose', 'kato_network_image_custom_sizes' );
+
+function kato_network_image_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'medium-large' => __('Medium Large'),
+    ) );
+}
